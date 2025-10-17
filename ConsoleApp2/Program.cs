f09bab2a3e9528ff068b402013ef165ccb9047a5
ramyaakuthota <ramyaakuthota19@gmail.com>
2025-10-17 15:01:46 +0530

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
-{
-    //add one more point
+namespace ConsoleA
     internal class Program
     {
         static void Main(string[] args)
